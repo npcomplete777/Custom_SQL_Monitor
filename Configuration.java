ceee21d9e7d1f34f69b9a52960115271b817eb1a
Aaron Jacobs <npcomplete29@gmail.com>
2015-07-23 20:49:56 +1000

    Create Configuration.java

---
+++ Configuration.java
@@ -0,0 +1,48 @@
+package com.singularity.ee.agent.systemagent.monitors;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Command 
+{
+
+    private String command;
+    private String displayPrefix; 
+    
+    private String timeStampCommand;
+    private String filePath;
+
+    public String getDisplayPrefix() {return displayPrefix;}
+
+    public void setDisplayPrefix(String displayPrefix) {this.displayPrefix = displayPrefix;}
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) 
+    {
+        this.command = command;
+    }
+    
+    public String getTimeStampCommand()
+    {
+    	return timeStampCommand;
+    }
+    
+    public void setTimeStampCommand(String timeStampCommand)
+    {
+    	this.timeStampCommand = timeStampCommand;
+    }
+    
+    public String getFilePath()
+    {
+    	return filePath;
+    }
+    
+    public void setFilePath(String filePath)
+    {
+    	this.filePath = filePath;
+    }
+
+}
